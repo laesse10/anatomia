9babea3ed321c2e54be6291c0107ce06b0c493bc
Lars Hulsbergen <laesse.hulsbergen@gmail.com>
2025-09-23 10:22:02 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import CallToAction from "@/components/call-to-action";
+import FAQsThree from "@/components/faqs-3";
+import Features from "@/components/features-1";
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import TeamSection from "@/components/team";
+
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <CallToAction />
+      <FAQsThree />
+      <FooterSection />
+    </>
+  );
+}
