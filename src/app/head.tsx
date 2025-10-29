0b31255f936beee2b67893f6f3ed3a58da8c62ca
Lars Hulsbergen <laesse.hulsbergen@gmail.com>
2025-10-29 11:11:55 +0100

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+      <script src="https://t.contentsquare.net/uxa/029cf532918a8.js"></script>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
